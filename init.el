a44629387a7ca83d4c0c388d9b75f47d839048fa
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2024-06-09 11:37:49 +0300

    small change

--- init.el
+++ init.el
@@ -21,8 +21,6 @@
  
  project-vc-merge-submodules nil
 
- dired-listing-switches "-agGh --group-directories-first"
-
  completion-show-help nil
  completions-max-height 20
  completion-auto-select 'second-tab
